8a4a82f42fe91df9728e657bc17be4526ab4d7f7
turevychvlad <v.o.turevych@student.khai.edu>
2025-02-02 15:39:15 +0300

    2-st stage

--- SignalProcessing/SignalProcessing.py
+++ SignalProcessing/SignalProcessing.py
@@ -1,22 +1,25 @@
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 
 # Параметри сигналу
-a = 0  # Середнє значення
-b = 10  # Стандартне відхилення
-n = 1000  # Кількість точок
+a = 0      # Середнє значення
+b = 10     # Стандартне відхилення
+n = 1000   # Кількість точок
+Fs = 1000  # Частота дискретизації (Гц)
 
 # Генерація випадкового сигналу
-signal = numpy.random.normal(a, b, n)
+signal = np.random.normal(a, b, n)
 
+# Визначення часу
+time = np.arange(n) / Fs  # Масив часу
 
 # Побудова графіка
 plt.figure(figsize=(10, 4))
-plt.plot(signal, label="Випадковий сигнал")
-plt.xlabel("Індекс")
+plt.plot(time, signal, label="Випадковий сигнал")
+plt.xlabel("Час (с)")
 plt.ylabel("Амплітуда")
-plt.title("Згенерований випадковий сигнал")
+plt.title("Згенерований випадковий сигнал у часовій області")
 plt.legend()
 plt.grid()
-plt.savefig("SignalProcessing/figures/random_signal.png")  # Збереження графіка
+plt.savefig("SignalProcessing/figures/random_signal_with_time.png")  # Збереження графіка
 plt.show()
