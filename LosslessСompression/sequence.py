9fc7e07c9e4fe970c41256ba9d1ae926e2b7608d
turevychvlad <v.o.turevych@student.khai.edu>
2025-03-10 21:13:20 +0300

    4-st stage

--- LosslessСompression/sequence.py
+++ LosslessСompression/sequence.py
@@ -20,15 +20,27 @@ def generate_sequence_3(surname, total_length=100):
     random.shuffle(sequence)
     return ''.join(sequence)
 
+def generate_sequence_4(surname, group_number, total_length=100):
+    letters = list(surname) + list(group_number)
+    n_letters = len(letters)
+    n_repeats = total_length // n_letters
+    remainder = total_length % n_letters
+    sequence = (letters * n_repeats) + letters[:remainder]
+    return ''.join(sequence)
 
-student_number = 13  #
+
+student_number = 13
 test_surname = "Смірнов"
+group_number = "529"
+
 
 original_sequence_1 = generate_sequence_1(student_number)
 original_sequence_2 = generate_sequence_2(test_surname)
 original_sequence_3 = generate_sequence_3(test_surname)
+original_sequence_4 = generate_sequence_4(test_surname, group_number)
 
-original_sequences = [original_sequence_1, original_sequence_2, original_sequence_3]
+
+original_sequences = [original_sequence_1, original_sequence_2, original_sequence_3, original_sequence_4]
 
 with open("LosslessСompression/results_sequence.txt", "a", encoding="utf-8") as file:
     for i, seq in enumerate(original_sequences, start=1):
