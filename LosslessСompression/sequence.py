c276284fd284ca015d90b877e839a68fdcf33629
turevychvlad <v.o.turevych@student.khai.edu>
2025-03-10 21:17:07 +0300

    5-st stage

--- LosslessСompression/sequence.py
+++ LosslessСompression/sequence.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 def generate_sequence_1(n1, total_length=100):
     list1 = ['1'] * n1
@@ -28,19 +29,25 @@ def generate_sequence_4(surname, group_number, total_length=100):
     sequence = (letters * n_repeats) + letters[:remainder]
     return ''.join(sequence)
 
+def generate_sequence_5(surname, group_number, total_length=100):
+    elements = list(surname[:2]) + list(group_number)
+    sequence = [random.choice(elements) for _ in range(total_length)]
+    random.shuffle(sequence)
+    return ''.join(sequence)
 
 student_number = 13
 test_surname = "Смірнов"
 group_number = "529"
 
-
 original_sequence_1 = generate_sequence_1(student_number)
 original_sequence_2 = generate_sequence_2(test_surname)
 original_sequence_3 = generate_sequence_3(test_surname)
 original_sequence_4 = generate_sequence_4(test_surname, group_number)
+original_sequence_5 = generate_sequence_5(test_surname, group_number)
 
+original_sequences = [original_sequence_1, original_sequence_2, original_sequence_3, original_sequence_4, original_sequence_5]
 
-original_sequences = [original_sequence_1, original_sequence_2, original_sequence_3, original_sequence_4]
+os.makedirs("LosslessСompression", exist_ok=True)
 
 with open("LosslessСompression/results_sequence.txt", "a", encoding="utf-8") as file:
     for i, seq in enumerate(original_sequences, start=1):
